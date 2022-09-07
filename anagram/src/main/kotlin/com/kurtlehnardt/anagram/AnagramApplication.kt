a8c90125ff7155308b0c2da5b9f4326237357bd9
Kurt Lehnardt <gutbookvonnegut@gmail.com>
2022-09-07 03:25:15 -0600

    iterate on business logic, cleaner version doesn't overwrite string variable

--- anagram/src/main/kotlin/com/kurtlehnardt/anagram/AnagramApplication.kt
+++ anagram/src/main/kotlin/com/kurtlehnardt/anagram/AnagramApplication.kt
@@ -6,77 +6,61 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 class AnagramApplication
 
 fun main(args: Array<String>) {
-    print("1 This should be true: " +  isAnagram("TomMarvoloRiddle","IAmLordVoldemort"))
+    print("1 This should be true: " +  isAnagramV2("TomMarvoloRiddle","IAmLordVoldemort"))
     print("\n")
-    print("2 This should be false: " + isAnagram("harrypotter","jarjarbinks"))
+    print("2 This should be false: " + isAnagramV2("harrypotter","jarjarbinks"))
     print("\n")
-    print("3 This should be true: " + "anagram, " + "nag a ram " + isAnagram("anagram","nag a ram"))
+    print("3 This should be true: " + "anagram, " + "nag a ram " + isAnagramV2("anagram","nag a ram"))
     print("\n")
-    print("4 This should be true: " + "below, " + "elbow " + isAnagram("below","elbow"))
+    print("4 This should be true: " + "below, " + "elbow " + isAnagramV2("below","elbow"))
     print("\n")
-    print("5 This should be true: " + "study, " + "dusty " + isAnagram("study","dusty"))
+    print("5 This should be true: " + "study, " + "dusty " + isAnagramV2("study","dusty"))
     print("\n")
-    print("6 This should be true: " + "night, " + "thing " + isAnagram("night","thing"))
+    print("6 This should be true: " + "night, " + "thing " + isAnagramV2("night","thing"))
     print("\n")
-    print("7 This should be true: " + "act, " + "cat " + isAnagram("act","cat"))
+    print("7 This should be true: " + "act, " + "cat " + isAnagramV2("act","cat"))
     print("\n")
-    print("8 This should be false, s is repeated twice,  "+ "dessert, " + "stressed " + isAnagram("dessert","stressed"))
+    print("8 This should be false, s is repeated twice,  "+ "dessert, " + "stressed " + isAnagramV2("dessert","stressed"))
     print("\n")
-    print("9 This should be true: " + "bad credit, " + "debit card " + isAnagram("bad credit","debit card"))
+    print("9 This should be true: " + "bad credit, " + "debit card " + isAnagramV2("bad credit","debit card"))
     print("\n")
-    print("10 This should be true: " + "gainly, " + "laying " + isAnagram("gainly","laying"))
+    print("10 This should be true: " + "gainly, " + "laying " + isAnagramV2("gainly","laying"))
     print("\n")
-    print("11 This should be true: " + "conversation, " + "voice rants on " + isAnagram("conversation","voice rants on"))
+    print("11 This should be true: " + "conversation, " + "voice rants on " + isAnagramV2("conversation","voice rants on"))
     print("\n")
-    print("12 This should be true: " + "eleven plus two, " + "twelve plus one " + isAnagram("eleven plus two","twelve plus one"))
+    print("12 This should be true: " + "eleven plus two, " + "twelve plus one " + isAnagramV2("eleven plus two","twelve plus one"))
     print("\n")
-    print("13 This should be true: " + "they see, " + "the eyes " + isAnagram("they see","the eyes"))
+    print("13 This should be true: " + "they see, " + "the eyes " + isAnagramV2("they see","the eyes"))
     print("\n")
-    print("14 This should be true: " + "funeral, " + "real fun " + isAnagram("funeral","real fun"))
+    print("14 This should be true: " + "funeral, " + "real fun " + isAnagramV2("funeral","real fun"))
     print("\n")
-    print("15 This should be true: " + "meteor, " + "remote " + isAnagram("meteor","remote"))
+    print("15 This should be true: " + "meteor, " + "remote " + isAnagramV2("meteor","remote"))
     print("\n")
-    print("16 This should be true: " + "the classroom, " + "schoolmaster " + isAnagram("the classroom","schoolmaster"))
+    print("16 This should be true: " + "the classroom, " + "schoolmaster " + isAnagramV2("the classroom","schoolmaster"))
     print("\n")
-    print("17 This should be true: " + "meal for one, " + "for me alone " + isAnagram("meal for one","for me alone"))
+    print("17 This should be true: " + "meal for one, " + "for me alone " + isAnagramV2("meal for one","for me alone"))
     print("\n")
-    print("18 This should be true: " + "sweep the floor, " + "too few helpers " + isAnagram("sweep the floor","too few helpers"))
+    print("18 This should be true: " + "sweep the floor, " + "too few helpers " + isAnagramV2("sweep the floor","too few helpers"))
     print("\n")
-    print("19 This should be true: " + "older and wiser, " + "I learned words " + isAnagram("older and wiser","I learned words"))
+    print("19 This should be true: " + "older and wiser, " + "I learned words " + isAnagramV2("older and wiser","I learned words"))
     print("\n")
-    print("20 This should be true: " + "video game, " + "give a demo " + isAnagram("video game","give a demo"))
+    print("20 This should be true: " + "video game, " + "give a demo " + isAnagramV2("video game","give a demo"))
     print("\n")
-    print("21 This should be true: " + "coins kept, " + "in pockets " + isAnagram("coins kept","in pockets"))
+    print("21 This should be true: " + "coins kept, " + "in pockets " + isAnagramV2("coins kept","in pockets"))
     print("\n")
-    print("22 This should be false, y is repeated twice, " + "young lady, " + "an old guy " + isAnagram("young lady","an old guy"))
+    print("22 This should be false, y is repeated twice, " + "young lady, " + "an old guy " + isAnagramV2("young lady","an old guy"))
 }
 
-/*
- * javascript reference
-  const isAnagramV2 = (word1, word2) => {
-  let isAnagram = false
-  let word1LowerCase = word1.toLowerCase().replace(/\s/g, '')
-  const letters = word2.toLowerCase().replace(/\s/g, '').split('')
-  if (word1LowerCase.length !== letters.length) return isAnagram
-  letters.map(letter => {
-    if (word1LowerCase.includes(letter)){
-      word1LowerCase = word1LowerCase.replace(letter, '')
-  })
-  if (word1LowerCase.length === 0){
-    isAnagram = true
-  }
-  return isAnagram
-
-  // I like this verison best
- const isAnagramv3 = (word1, word2) => {
-  const arr1 = word1.toLowerCase().replace(/\s/g, '').split('').sort()
-  const arr2 = word2.toLowerCase().replace(/\s/g, '').split('').sort()
-  let isAnagram = false
-  if (arr1.length !== arr2.length) return isAnagram
-  if (arr1.every(char => arr2.includes(char)) && arr1.length === arr2.length) isAnagram =  true
-  return isAnagram
+fun isAnagramV2(word1: String, word2: String): Boolean {
+    val letters1: List<String> = word1.lowercase().replace("\\s+".toRegex(), "").split("").sorted()
+    val letters2: List<String> = word2.lowercase().replace("\\s+".toRegex(), "").split("").sorted()
+    var isAnagram: Boolean = false
+    if (letters1.size != letters2.size) return isAnagram
+    if (letters1.all { letters2.contains(it) && letters1.size == letters2.size }){
+        isAnagram = true
+    }
+    return isAnagram
 }
-  */
 
 fun isAnagram(word1: String, word2: String): Boolean {
     var validAnagram: Boolean = false
