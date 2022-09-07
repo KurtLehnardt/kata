55f12fab4d551831f050fda7b316b2e1b7b9b286
Kurt Lehnardt <gutbookvonnegut@gmail.com>
2022-09-07 13:57:01 -0600

    update requestBody

--- anagram/src/main/kotlin/com/kurtlehnardt/app/anagram/AnagramController.kt
+++ anagram/src/main/kotlin/com/kurtlehnardt/app/anagram/AnagramController.kt
@@ -1,18 +1,19 @@
 package com.kurtlehnardt.app.anagram
 
+import com.fasterxml.jackson.databind.util.JSONPObject
 import org.springframework.web.bind.annotation.*
 
 @RestController
 // Omitted @Autowired annotation as per section 5 https://www.baeldung.com/kotlin/spring-boot-kotlin
 class AnagramController(val anagramService: AnagramService) {
     @GetMapping("/")
-    fun yandex() = "yo"
+    fun index() = "Hello World"
     @PostMapping("/checkAnagram")
-    fun index() = "hello"
-//    @ResponseBody
-//    fun checkAnagramPost(@PathVariable words: String): Boolean {
-//        var word1: String = words.split(',')[0]
-//        var word2: String = words.split(',')[1]
-//        return anagramService.isAnagram(word1, word2)
-//    }
+    @ResponseBody
+    fun checkAnagramPost(@RequestBody words: String): Boolean {
+        if (words == null || words.isEmpty()) { return false}
+        var word1: String = words.split(",")[0]
+        var word2: String = words.split(",")[1]
+        return anagramService.isAnagram(word1, word2)
+    }
 }
