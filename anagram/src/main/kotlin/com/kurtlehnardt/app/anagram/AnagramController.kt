e93c5d643da9e8d5598041d01b03fce737c2e4ab
Kurt Lehnardt <gutbookvonnegut@gmail.com>
2022-09-07 15:46:59 -0600

    change return val to object

--- anagram/src/main/kotlin/com/kurtlehnardt/app/anagram/AnagramController.kt
+++ anagram/src/main/kotlin/com/kurtlehnardt/app/anagram/AnagramController.kt
@@ -7,13 +7,13 @@ import org.springframework.web.bind.annotation.*
 // Omitted @Autowired annotation as per section 5 https://www.baeldung.com/kotlin/spring-boot-kotlin
 class AnagramController(val anagramService: AnagramService) {
     @GetMapping("/")
-    fun index() = "Hello World"
+    fun index() = "{result: true}"
     @PostMapping("/checkAnagram")
     @ResponseBody
-    fun checkAnagramPost(@RequestBody words: String): Boolean {
-        if (words == null || words.isEmpty()) { return false}
+    fun checkAnagramPost(@RequestBody words: String): String {
+        if (words == null || words.isEmpty()) { return "{result: ${false}"}
         var word1: String = words.split(",")[0]
         var word2: String = words.split(",")[1]
-        return anagramService.isAnagram(word1, word2)
+        return "{result: ${anagramService.isAnagram(word1, word2)}}"
     }
 }
