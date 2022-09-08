39baf9be62829a3641dbdfd6f1fa7c5c08c90d08
Kurt Lehnardt <gutbookvonnegut@gmail.com>
2022-09-07 22:21:42 -0600

    add dependency for JSON, change return value type to JSON object

--- anagram/src/main/kotlin/com/kurtlehnardt/app/anagram/AnagramController.kt
+++ anagram/src/main/kotlin/com/kurtlehnardt/app/anagram/AnagramController.kt
@@ -1,19 +1,22 @@
 package com.kurtlehnardt.app.anagram
 
-import com.fasterxml.jackson.databind.util.JSONPObject
+import com.beust.klaxon.*
 import org.springframework.web.bind.annotation.*
 
 @RestController
 // Omitted @Autowired annotation as per section 5 https://www.baeldung.com/kotlin/spring-boot-kotlin
 class AnagramController(val anagramService: AnagramService) {
     @GetMapping("/")
-    fun index() = "{result: true}"
+    fun index() = "Hello World!"
+
     @PostMapping("/checkAnagram")
     @ResponseBody
-    fun checkAnagramPost(@RequestBody words: String): String {
-        if (words == null || words.isEmpty()) { return "{result: ${false}"}
-        var word1: String = words.split(",")[0]
-        var word2: String = words.split(",")[1]
-        return "{result: ${anagramService.isAnagram(word1, word2)}}"
+    fun checkAnagramPost(@RequestBody words: String): Any {
+        val parser: Parser = Parser.default()
+        if (words == null || words.isEmpty()) {
+            return parser.parse(StringBuilder("""{"result": "Please check inputs" }""")) as JsonObject
+        }
+        val result = anagramService.isAnagram(words.split(",")[0], words.split(",")[1])
+        return parser.parse(StringBuilder("""{"result": $result }""")) as JsonObject
     }
 }
