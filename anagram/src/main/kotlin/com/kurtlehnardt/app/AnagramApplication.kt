4d18e54f4b43f3acb14a89e67085036da1d3e52c
Kurt Lehnardt <gutbookvonnegut@gmail.com>
2022-09-07 22:23:29 -0600

    add TODO

--- anagram/src/main/kotlin/com/kurtlehnardt/app/AnagramApplication.kt
+++ anagram/src/main/kotlin/com/kurtlehnardt/app/AnagramApplication.kt
@@ -8,6 +8,12 @@ class AnagramApplication
 
 fun main(args: Array<String>) {
     runApplication<AnagramApplication>( * args)
+    /*
+    TODO
+    a.       Write Unit Tests against the Spring Service
+    d.       Add a Swagger User Interface to be able to more easily interact with the endpoint
+    e.       Add authentication to the API (either Basic or OAuth)
+     */
 }
 
 
